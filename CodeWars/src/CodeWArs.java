ff10aacb7dd6f0b57fea2deda6eda1111aa38510
vladey <bagdeyv@gmail.com>
2017-09-28 17:12:53 +0300

    New

--- CodeWars/src/CodeWArs.java
+++ CodeWars/src/CodeWArs.java
@@ -1,6 +1,6 @@
 public class CodeWArs {
 
     public static void main(String[] args) {
-
+        System.out.println(TenMinWalk.isValid(new char[] {'n','n','n','s','n','s','n','s','n','s'}));
     }
 }
